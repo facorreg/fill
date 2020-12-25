17c8e46ecd950992239835e79dfde1a8463ad54c
root <root@DESKTOP-51MGIR3.localdomain>
2020-12-25 22:39:02 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+const webpack = require('webpack');
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+const config = {
+  entry: './test.js',
+  resolve: {
+    extensions: ['.mjs', '.js'],
+    alias: {
+      // utils: path.resolve(__dirname, 'src/utils/'),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  target: 'node',
+  plugins: [new NodemonPlugin()],
+};
+
+module.exports = config;
